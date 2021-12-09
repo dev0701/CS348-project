bfc68ce02a92ba86ab7fa5995c5a3e68198ae01b
vamsikoll <kolluri@purdue.edu>
2021-12-08 19:17:52 -0500

    update employee partially working

--- main.py
+++ main.py
@@ -81,6 +81,63 @@ def search():
             return render_template("search.html", data=data)       
     return render_template("search.html")
 
+@app.route("/edit", methods=['POST','GET'])
+def edit():
+    if not session.get('logged_in'):
+        return render_template("login.html")
+    else:
+       if request.method == "POST":
+            print('post method')
+            form = request.form
+            search_value = form['search_string']
+            print(search_value)
+            conn = mysql.connect()
+            with conn.cursor() as cursor: 
+                result = cursor.execute('SELECT DISTINCT * FROM Employee WHERE employee_id = %s OR first_name = %s OR last_name = %s OR position = %s OR salary = %s OR department_id = %s OR city = %s', (search_value,search_value,search_value,search_value,search_value,search_value,search_value ))
+            data = cursor.fetchall()
+            print(data)       
+            conn.close()
+            return render_template("edit.html", data=data)       
+    return render_template("edit.html")
+
+@app.route("/edit_employee", methods=['POST','GET'])
+def edit_employee_form():
+    if not session.get('logged_in'):
+        return render_template("login.html")
+    else:
+        if request.method == "POST":
+            print('post method')
+            search_string = request.form['action']
+            conn = mysql.connect()
+            with conn.cursor() as cursor: 
+                result = cursor.execute('SELECT DISTINCT * FROM Employee WHERE employee_id = %s ',(search_string))
+            data = cursor.fetchall()  
+            session['emp_id'] = data[0][0]   
+            conn.close()
+            return render_template("edit_employee.html", data=data)       
+    return render_template("edit.html")
+  
+
+@app.route("/employee_edited", methods=['POST','GET'])
+def edit_employee():
+    conn = mysql.connect()
+    with conn.cursor() as cursor:
+        emp_id = session.get('emp_id', None)
+        result = cursor.execute('UPDATE EMPLOYEE SET first_name = %s, last_name = %s WHERE employee_id = %s ', (request.form['first-name'], request.form['last-name'], emp_id,))
+        conn.commit()
+        if result > 0:
+            return login()
+        else:
+            useradded = 0
+    conn.close()
+    return edit_employee_form()
+
+
+    
+            
+        
+        
+
 @app.route("/adduser", methods=['POST','GET'])
 def addUserForm():
     return render_template("addUser.html")
